25731e95d238a39c3271834139baca44d1a3f23e
NgoMike <mqngo21@gmail.com>
2018-01-26 22:34:54 -0800

    .....

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,30 @@
 var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
+  username: this.username,
+  text: this.text,
+  roomname: this.roomname
 };
 
 
 var app = {
-  
-  constructor: function() {
-    this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
-  },
+  server: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+ 
   
   init: function () {
-    
+    setInterval(function() {
+      this.fetch();
+    });
+   
   },
+  
+  // renderMessage: function(messages) {
+  //   for (var i = 0; i < messages.length; i++) { 
+  //     if (messages.results[i].username === undefined || messages.results[i].text.length === 0 || messages.results[i].username.length === 0 ) {
+          
+  //     } else {
+  //       $('#chats').append('<p>' + messages.results[i].username + ':' + '<br> <br>' + messages.results[i].text + '</p>');
+  //     }
+  //   }
+  // },
 
   send: function (message) {
     $.ajax({
@@ -32,21 +43,22 @@ var app = {
   },
 
   fetch: function () {
-
     $.ajax({
       url: this.server,  
       type: 'GET',
-      data: message,
-      contentType: 'json',
+      data: 'order=-createdAt&limit=100',
       success: function (data) {
-        console.log('chatterbox: Message sent', data);
+        app.renderMessage(data);
+        console.log('Fetch Success', data.results);
       },
       error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
+        console.error('Fetch Failed', data);
       }
     });  
   },
+  
+  
   
   clearMessages: function () {
     $('#chats').children().remove(); //this currently clears everything!
@@ -69,8 +81,8 @@ var app = {
   },
   
   
-  handleUser: function (e) {
-    $('.createUser').click(function(e) {
+  handleUser: function () {
+    $('.createUser').click(function() {
       app.renderUser();
     }); 
   },
@@ -79,9 +91,9 @@ var app = {
     var nameValue = $('#user').val();
     $('#names').append('<li "value=user">' + nameValue + '</li>');
   }
+};
  
 
-};
 
-// $(document).ready(function(handleUser, handleSubmit);
+
   
